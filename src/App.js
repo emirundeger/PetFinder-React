9762838085db85a9c9d6eb3d7f3a3ce9e550c6f4
emir.undeger <emirundeger@gmail.com>
2019-11-08 03:07:56 +0000

    Getting the data from the API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import { render } from "react-dom";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+
+    promise.then(console.log, console.error); //if it's successful console the data we got back from the api, if there is an error
+  }
   render() {
-    return React.createElement("div", {}, []);
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </div>
+    );
   }
 }
+
+render(<App />, document.getElementById("root"));
